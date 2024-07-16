41a0c63214ff1ed8dd134b5b208e0a4f51d29ec1
HaiNL2804 <151650351+HaiNL2804@users.noreply.github.com>
2024-07-16 17:04:21 +0700

    Đã cắt rõ các phần trong admin he ho fo dùng chung, đã lia link các trang php

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,56 @@
 <?php
+    include "layout/header.php";
+    include "layout/box_left.php";
+ 
+    if(isset($_GET['act'])){
+        switch ($_GET['act']) {
+   
+            case 'buttons':
+                include "./view/buttons.php";
+                break;
+            case 'cards':
+                include "./view/cards.php";
+                break;
+            case 'color': //utilities-color
+                include "./view/utilities-color.php";
+                break;
+            case 'border': //utilities-border
+                include "./view/utilities-border.php";
+                break;
+            case 'utilities-animation': //utilities-animation
+                include "./view/utilities-animation.php";
+                break;
+            case 'other': //utilities-other
+                include "./view/utilities-other.php";
+                break;
+            case 'login': // Dang Nhap
+                include "./view/login.php";
+                break;
+            case 'register': //dang ky
+                include "./view/register.php";
+                break;
+            case 'forgot-password': // Quen mat khau
+                include "./view/forgot-password.php";
+                break;
+            case '404': 
+                include "./view/404.php";
+                break;
+            case 'blank': 
+                include "./view/blank.php";
+                break;
+            case 'charts': 
+                include "./view/charts.php";
+                break;
+            case 'tables': 
+                include "./view/tables.php";
+                break;
 
+            default:
+            include "./layout/home.php";
+                break;
+        }
+    }else{
+        include "layout/home.php";
+    }
+    include "layout/footer.php";
 ?>
