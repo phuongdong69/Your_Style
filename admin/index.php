fbc5d190a3793b2eaa00e0f1e33cf481273822a3
phuongdong69 <phuongdongdragon@gmail.com>
2024-07-21 20:23:13 +0700

    thêm ảnh

--- admin/index.php
+++ admin/index.php
@@ -3,19 +3,19 @@ include "layout/header.php";
 include "layout/box_left.php";
 
 // Sửa đường dẫn nếu cần thiết
-include "../Model/pdo.php"; 
+include "../Model/pdo.php";
 include "../Model/product.php";
 include "../Model/category.php";
-
+include "../Model/galery.php";
 if (isset($_GET['act'])) {
     switch ($_GET['act']) {
-        //Danh sách sản phẩm
+            //Danh sách sản phẩm
         case 'listsp': //done show
             $listSanPham = load_product("", 0);
             include "./view/sanpham/listsp.php";
             break;
         case 'addsp': //done thêm sản phẩm
-            if(isset($_POST['themmoi'])&&($_POST['themmoi'])){
+            if (isset($_POST['themmoi']) && ($_POST['themmoi'])) {
                 $name = $_POST['name'];
                 $description = $_POST['description'];
                 $price = $_POST['price'];
@@ -26,71 +26,113 @@ if (isset($_GET['act'])) {
                 $update_at = $_POST['update_at'];
                 $id_cate = $_POST['id_cate'];
                 $id_brands = $_POST['id_brands'];
-                insert_product($name, $description, $price, $priceSale, $quantity, $status, $create_at, $update_at , $id_cate, $id_brands);
-            $thongbao="Thêm thành công";
+                insert_product($name, $description, $price, $priceSale, $quantity, $status, $create_at, $update_at, $id_cate, $id_brands);
+                $thongbao = "Thêm thành công";
             }
             $listdanhmuc = load_category();
             include "./view/sanpham/addsp.php";
             break;
         case 'updatesp': //sửa done
-                if (isset($_GET['id']) && $_GET['id'] > 0) {
-                    $product = load_one_product($_GET['id']);
-                    extract($product);
-                }
-                if (isset($_POST['capnhatsanpham'])) {
-                    $id = $_POST['id'];
-                    $name = $_POST['name'];
-                    $description = $_POST['description'];
-                    $price = $_POST['price'];
-                    $priceSale = $_POST['priceSale'];
-                    $quantity = $_POST['quantity'];
-                    $status = $_POST['status'];
-                    $update_at = $_POST['update_at'];
-                    $id_cate = $_POST['id_cate'];
-                    $id_brands = $_POST['id_brands'];
-                    update_product($name, $description, $price, $priceSale, $quantity, $status, $update_at, $id_cate, $id_brands, $id);
-                    $thongbao = "Cập nhật thành công";
-                }
-                $listdanhmuc = load_category();
-        include "./view/sanpham/updatesp.php";
-        break;
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $product = load_one_product($_GET['id']);
+                extract($product);
+            }
+            if (isset($_POST['capnhatsanpham'])) {
+                $id = $_POST['id'];
+                $name = $_POST['name'];
+                $description = $_POST['description'];
+                $price = $_POST['price'];
+                $priceSale = $_POST['priceSale'];
+                $quantity = $_POST['quantity'];
+                $status = $_POST['status'];
+                $update_at = $_POST['update_at'];
+                $id_cate = $_POST['id_cate'];
+                $id_brands = $_POST['id_brands'];
+                update_product($name, $description, $price, $priceSale, $quantity, $status, $update_at, $id_cate, $id_brands, $id);
+                $thongbao = "Cập nhật thành công";
+            }
+            $listdanhmuc = load_category();
+            include "./view/sanpham/updatesp.php";
+            break;
 
-        //Danh mục sản phẩm
+            //Danh mục sản phẩm
         case 'listdm': //done danh mục
-            $listcategory = load_category("", 0); 
+            $listcategory = load_category("", 0);
             include "./view/danhmuc/listdm.php";
             break;
         case 'adddm':
             if (isset($_POST['themdanhmuc']) && ($_POST['themdanhmuc'])) {
                 $name = $_POST['name'];
                 $status = $_POST['status'];
-                insert_category($name,$status);
+                insert_category($name, $status);
                 $thongbao = "Thêm thành công";
-            } 
+            }
             include "./view/danhmuc/adddm.php";
             break;
-            case 'updatedm':
-                if (isset($_GET['id']) && $_GET['id'] > 0){
-                    $danhmuc = load_one_category($_GET['id']);
-                    extract($danhmuc);
-                }
+        case 'updatedm':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $danhmuc = load_one_category($_GET['id']);
+                extract($danhmuc);
+            }
+
+            if (isset($_POST['capnhatdanhmuc'])) {
+                $name = trim($_POST['name']);
+                $status = trim($_POST['status']);
+                update_category($name, $status, $_GET['id']);
+                $thongbao = "Cập nhật thành công";
+                $danhmuc = load_one_category($_GET['id']);
+                extract($danhmuc);
+            }
+            $listdanhmuc = load_category();
+            include "./view/danhmuc/updatedm.php";
+            break;
+        // case 'showimg':
+        // if(isset($_GET['id']) && $_GET['id'] > 0){
+        //     $listproduct=load_one_product($id);
+        //     extract($listproduct);
+        //     $id_product = $id;
+        // }
             
-                if (isset($_POST['capnhatdanhmuc'])){
-                    $name = trim($_POST['name']);
-                    $status = trim($_POST['status']);
-                    update_category($name, $status, $_GET['id']);
-                    $thongbao = "Cập nhật thành công";
-                    $danhmuc = load_one_category($_GET['id']);
-                    extract($danhmuc);
-                }
-                $listdanhmuc = load_category();
-                include "./view/danhmuc/updatedm.php";
-                break;
+        //     $listanh = load_galery($id_product);
+
+        //     include "./view/galery/showimg.php";
+        //     break;
+        case 'showimg':
             
+        
+            $listanh = load_galery();
+        
+            include "./view/galery/showimg.php";
+            break;
+        
+        case 'addimg':
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $pro = load_one_product($_GET['id']);
+                extract($pro);
+            }
+            if (isset($_POST['themanh'])) {
+                
+                // Xử lý hình ảnh 
+                $image = $_FILES['image']['name'];
+                $target_dir = "./img/";
+                $target_file = $target_dir . basename($image);
+                if (move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
+                    echo "upload thành công";
+                } else {
+                    echo "Có lỗi trong quá trình upload file";
+                }
+                $id_product = $_POST['id_product'];
+                // Gọi model để thực hiện câu lệnh insert
+                insert_galery($image,$id_product);
+                $thongBao = "Thêm sản phẩm thành công";
+            }
+            // $listanh = load_galery($_GET['id_product']);
+            // $listproduct = load_product();
+            include "./view/galery/add.php";
+            break;
     }
 } else {
     include "layout/home.php";
     //ố dè
 }
 include "layout/footer.php";
-?>
---
+++ admin/view/galery/showimg.php
@@ -0,0 +1,47 @@
+<article>
+    <div class="row2">
+        <div class="row2 font_title">
+            <h1>Hình ảnh sản phẩm</h1>
+        </div>
+        <div class="row2 form_content ">
+            <form action="index.php?act=showimg&id=<?= $id_product ?>" method="POST">
+                <div class="row2 mb10 formds_loai">
+                    <table border="1">
+                        <tr>
+                            <th>Mã</th>
+                            <th>Hình ảnh</th>
+                            <th>Mã sản phẩm</th>
+                            <th></th>
+                        </tr>
+                        <?php
+                        foreach ($listanh as $gallery) {
+                            extract($gallery);
+                            $imgPath = "../img/" . $image;
+                            $id_product = $id;
+                            $updateimg = "?act=updateimg&id_product=" . $id_product;
+                            $addimg_pro = "?act=addimg&id_product=" . $id_product;
+                            
+                        ?>
+                            <tr>
+                                <td><?= $id ?></td>
+                                <td>
+                                    <img src="<?= $imgPath ?>" alt="" width="100px" height="100px">
+                                </td>
+                                <td><?= $id_product ?></td>
+                                <td>
+                                    <a href="<?= $updateimg ?>">
+                                        <input type="button" value="Sửa">
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                </div>
+                <div class="row mb10">
+                    <a href="<?= $addimg_pro ?>"><input class="mr20" type="button" value="Thêm ảnh"></a>
+                    <a href="index.php?act=listsp"><input class="mr20" type="button" value="Danh sách sản phẩm"></a>
+                </div>
+            </form>
+        </div>
+    </div>
+</article>
