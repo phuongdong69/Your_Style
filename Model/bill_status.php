6d6a3a91ff1c7ec51cf9ee572a41c1366b61fd75
lethevinh17904 <vinhkotuoi@gmail.com>
2024-07-22 14:34:21 +0700

    update bill status va 1 phan bill detail

--- Model/bill_status.php
+++ Model/bill_status.php
@@ -1,14 +1,14 @@
 <?php 
 function insert_bill_status($status){
-    $sql = "INSERT INTO `bill_status` ( `id`,`status`) 
-    VALUES (NULL, '$status');";
+    $sql = "INSERT INTO `bill_status` (`id`, `status`) VALUES (NULL, '$status');";
     return pdo_execute($sql);
 }
+
 function load_bs(){
     $sql = "SELECT * FROM `bill_status`";
-    // $listbrands = pdo_query($sql);
     return pdo_query($sql);
 }
+
 function update_bs($status, $id){
     $sql = "UPDATE `bill_status` SET `status` = '$status' WHERE `id` = $id;";
     return pdo_execute($sql);
@@ -18,4 +18,3 @@ function load_one_bs($id){
     $sql = "SELECT * FROM `bill_status` WHERE `id`=$id;";
     return pdo_query_one($sql);
 }
-?>
