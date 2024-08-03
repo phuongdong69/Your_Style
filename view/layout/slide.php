ff9de994b7706631f330c542602e7c15b752e1e9
HaiNL2804 <151650351+HaiNL2804@users.noreply.github.com>
2024-08-03 14:48:59 +0700

    sửa user

--- view/layout/slide.php
+++ view/layout/slide.php
@@ -12,7 +12,7 @@
                <div class="flat-caption caption3 formLeft delay500 text-center">
                </div>
                <div class="flat-button caption4 formLeft delay600 text-center"><a class="more" href="#">Chi Tiết Hơn</a></div>
-               <div class="flat-image formBottom delay200" data-duration="5" data-bottom="true"><img src="images/slider-image-01.png" alt=""></div>
+               <div class="flat-image formBottom delay200" data-duration="5" data-bottom="true"><img src="/images/banner1.jpg" alt=""></div>
             </li>
             <li>
                <div class="flat-caption caption2 formLeft delay400">
@@ -22,7 +22,7 @@
                   <h2><br>Nơi phong cách bắt đầu</h2>
                </div>
                <div class="flat-button caption5 formLeft delay600"><a class="more" href="#">Chi Tiết Hơn</a></div>
-               <div class="flat-image formBottom delay200" data-bottom="true"><img src="images/slider-image-02.png" alt=""></div>
+               <div class="flat-image formBottom delay200" data-bottom="true"><img src="/images/banner2.jpg" alt=""></div>
             </li>
             <li>
                <div class="flat-caption caption2 formLeft delay400 text-center">
@@ -32,7 +32,7 @@
                   <p><br>Đơn giản tạo sự sang trọng</p>
                </div>
                <div class="flat-button caption4 formLeft delay600 text-center"><a class="more" href="#">Chi Tiết Hơn</a></div>
-               <div class="flat-image formBottom delay200" data-bottom="true"><img src="images/slider-image-03.png" alt=""></div>
+               <div class="flat-image formBottom delay200" data-bottom="true"><img src="/images/banner3.jpg" alt=""></div>
             </li>
          </ul>
       </div>
