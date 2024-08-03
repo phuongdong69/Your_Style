0e18613d726bf35a9bac82e35eb84df2b78c518a
HaiNL2804 <151650351+HaiNL2804@users.noreply.github.com>
2024-08-03 20:00:39 +0700

    sửa lại giao diện, giao diện trang productdetail, thêm file js, lia link bên heade,

--- view/layout/footer.php
+++ view/layout/footer.php
@@ -51,13 +51,13 @@
             </div>
          </div>
       </div>
-      <!-- Bootstrap core JavaScript==================================================-->
-	  <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+      <!-- Loại bỏ Bootstrap core JavaScript==================================================-->
+	  <!-- <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
 	  <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 	  <script type="text/javascript" src="js/bootstrap.min.js"></script>
 	  <script type="text/javascript" src="js/jquery.sequence-min.js"></script>
 	  <script type="text/javascript" src="js/jquery.carouFredSel-6.2.1-packed.js"></script>
 	  <script defer src="js/jquery.flexslider.js"></script>
-	  <script type="text/javascript" src="js/script.min.js" ></script>
+	  <script type="text/javascript" src="js/script.min.js" ></script> -->
    </body>
 </html>
